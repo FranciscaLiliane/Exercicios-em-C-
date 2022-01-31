e42195c88d7fba38044739553988f29f90bcc6da
FranciscaLiliane <42254398+FranciscaLiliane@users.noreply.github.com>
2022-01-31 16:45:17 -0300

    Add files via upload

---
+++ Exercicio4/Exercicio4.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio4
+{
+    class Exercicio4
+    {
+        static void Main(string[] args)
+        {
+            int numF, horas;
+            double valor, salario;
+              
+            Console.WriteLine("Digite o numero do funcionário: ");
+            numF = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a quantidade de horas trabalhadas: ");
+            horas = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite  o valor que recebe por: ");
+            valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            salario = horas * valor;
+
+            Console.WriteLine($"Number = {numF}");
+            Console.WriteLine($"Salary = U$ {salario.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
