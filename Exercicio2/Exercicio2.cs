e6bda63a94069c27673e2e64a980186749489c7c
FranciscaLiliane <42254398+FranciscaLiliane@users.noreply.github.com>
2022-01-31 16:46:27 -0300

    Add files via upload

---
+++ Exercicio2/Exercicio2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+namespace Exercicio2
+{
+    class Exercicio2
+    {
+        static void Main(string[] args)
+        {
+            double pi = 3.14159;
+
+            Console.WriteLine("Digite qual o raio do circulo: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double A = pi * Math.Pow(raio, 2.0);
+
+            Console.WriteLine($"A = {A.ToString("F4",CultureInfo.InvariantCulture)}");
+        }
+    }
+}
---
+++ Exercicio3/Exercicio3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercicio3
+{
+    class Exercicio3
+    {
+        static void Main(string[] args)
+        {
+            int A, B, C, D;
+
+            Console.WriteLine("Digite o valor de A: ");
+            A = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor de B: ");
+            B = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor de C: ");
+            C = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor de D: ");
+            D = int.Parse(Console.ReadLine());
+
+            int diferenca = (A * B - C * D);
+
+            Console.WriteLine($"Diferença = {diferenca}");
+        }
+    }
+}
